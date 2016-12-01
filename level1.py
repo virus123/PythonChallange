16848bdee0256e13793387ae4a741e225e200493
virus123 <kid1007@yahoo.co.in>
2016-12-01 18:57:02 +0530

    Level 1 solution

---
+++ level1.py
@@ -0,0 +1,4 @@
+data = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle grgl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+change= str.maketrans("abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxyzab")
+final = data.translate(change)
+print(final)
